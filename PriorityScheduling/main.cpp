655317e66f870435979c5733bf5cef98475e584d
Tushar Mann <tusharmann29@gmail.com>
2016-07-01 23:38:59 +0530

    Non-preemitive priority scheduling

---
+++ PriorityScheduling/main.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+struct job
+{
+int id;
+int art,brt,rt,prior;
+int ct,tat,wt;
+};
+bool cmp(struct job x,struct job y)
+{
+  return(x.art<y.art);
+
+}
+int main()
+{
+ int n,i,smallest,remain=0,time,tot=0;
+ float wait=0,turn=0;
+ float avgtat,avgwt;
+ job j[10];
+ cout<<"\nEnter number of jobs:";
+ cin>>n;
+ for(i=1;i<=n;i++)
+ {
+  j[i].id=i;
+ cout<<"\nEnter the priority of the process p"<<i<<" ";
+ cin>>j[i].prior;
+ cout<<"\nEnter arrival time of the process p"<<i<<" ";
+ cin>>j[i].art;
+ cout<<"\nEnter burst time of the process p"<<i<<" ";
+ cin>>j[i].brt;
+ j[i].rt=j[i].brt;
+
+ }
+ sort(j+1,j+n+1,cmp);
+
+ j[9].prior=9999;
+
+
+ for(time=0;remain!=n;time++)
+ {
+     smallest=9;
+
+  for(i=1;i<=n;i++)
+   {
+     if(j[i].art<=time && j[i].prior<j[smallest].prior && j[i].rt>0)
+        smallest=i;
+   }
+   if(smallest!=9)
+     j[smallest].rt=0;
+
+   if(j[smallest].rt==0)
+   {
+       remain++;
+     tot=time+j[smallest].brt;
+     time+=j[smallest].brt-1;
+     j[smallest].ct=tot;
+     j[smallest].tat=j[smallest].ct-j[smallest].art;
+     j[smallest].wt=j[smallest].tat-j[smallest].brt;
+     turn+=j[smallest].tat;
+     wait+=j[smallest].wt;
+   }
+
+ }
+
+
+ avgtat=turn/n;
+ avgwt=wait/n;
+
+ cout<<"\nAverage turn around time="<<avgtat;
+ cout<<"\nAverage waiting time="<<avgwt;
+
+
+  return 0;
+}
