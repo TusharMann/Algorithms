e3b29c52b92d77102cd5b59ab8f48b6bd28c0d27
Tushar Mann <tusharmann29@gmail.com>
2016-07-08 23:55:39 +0530

    Palindrome list in progress

--- Palindrome_List/main.cpp
+++ Palindrome_List/main.cpp
@@ -2,6 +2,40 @@
 
 using namespace std;
 
+
+struct node
+{
+    int info;
+    struct node *next;
+
+};
+ struct node *front;
+ struct node *rear;
+
+void push()
+{
+    struct node *ptr;
+    ptr=(struct node *)malloc(sizeof(struct node));
+    printf("\nEnter info ");
+    scanf("%d",&ptr->info);
+
+    if(front==NULL)
+    {
+        front=ptr;
+        rear=ptr;
+        ptr->next=NULL;
+    }
+
+    else
+    {
+        rear->next=ptr;
+        ptr->next=NULL;
+        rear=ptr;
+    }
+    printf("\nOne record Added ");
+}
+
+
 int main()
 {
     cout << "Hello world!" << endl;
