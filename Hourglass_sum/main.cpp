8eaa9983f106bc252a5316ac8d2b4d7145e90f3f
Tushar Mann <tusharmann29@gmail.com>
2016-07-13 00:01:27 +0530

    Working on hourglass sum

---
+++ Hourglass_sum/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[6][6],i,j,sum,s,a,b,c,d,e,f,g;
+    for(i=0;i<6;i++)
+    {
+        for(j=0;j<6;j++)
+        {
+            cin>>arr[i][j];
+        }
+
+    }
+
+
+    return 0;
+}
