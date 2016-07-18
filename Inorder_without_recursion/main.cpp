30543d05fec326d50166f43393c8ece48dd7eec1
Tushar Mann <tusharmann29@gmail.com>
2016-07-19 02:07:54 +0530

    Working on inorder traversal without recursion

--- Inorder_without_recursion/main.cpp
+++ Inorder_without_recursion/main.cpp
@@ -1,9 +1,218 @@
 #include <iostream>
+#include<queue>
+#include<stack>
 
 using namespace std;
 
-int main()
+struct node
 {
-    cout << "Hello world!" << endl;
-    return 0;
+	int data;
+    node *left,*right;
+};
+
+struct node *root=NULL;
+
+void add()
+{
+    node *ptr,*pre,*temp;
+	ptr=new node;
+    cout<<"\nEnter info ";
+	cin>>ptr->data;
+
+	ptr->left=NULL;
+	ptr->right=NULL;
+
+	if(root==NULL)
+	{
+		root=ptr;
+
+	}
+
+	else
+	{
+		temp=root;
+		pre=root;
+
+		while(temp!=NULL)
+		{
+			pre=temp;
+
+			if(temp->data>ptr->data)
+			temp=temp->left;
+
+			else
+			temp=temp->right;
+		}
+
+		if(pre->data>ptr->data)
+		pre->left=ptr;
+
+		else
+		pre->right=ptr;
+
+		cout<<"\nOne record added";
+	}
+
 }
+
+void inorder(node *ptr)
+{
+	if(ptr!=NULL)
+	{
+		inorder(ptr->left);
+		cout<<"\t"<<ptr->data;
+		inorder(ptr->right);
+
+	}
+
+}
+
+void preorder(struct node *ptr)
+{
+	if(ptr!=NULL)
+	{
+		cout<<"\t"<<ptr->data;
+		preorder(ptr->left);
+		preorder(ptr->right);
+
+	}
+
+}
+
+void postorder(struct node *ptr)
+{
+	if(ptr!=NULL)
+	{
+		postorder(ptr->left);
+		postorder(ptr->right);
+        cout<<"\t"<<ptr->data;
+	}
+
+}
+
+void LevelOrder(node *root)
+{
+    queue<node *> q;
+
+    if(root==NULL)
+        return;
+
+    else{
+
+    cout<<"\n";
+    cout<<root->data<<"\t";
+
+    if(root->left!=NULL)
+        q.push(root->left);
+
+    if(root->right!=NULL)
+        q.push(root->right);
+
+    while(!q.empty())
+    {
+        node *ptr=q.front();
+        cout<<ptr->data<<" ";
+        q.pop();
+
+    if(ptr->left!=NULL)
+        q.push(ptr->left);
+
+    if(ptr->right!=NULL)
+        q.push(ptr->right);
+
+    }
+
+  }
+
+}
+
+void Inorder_without_recursion(node *root)
+{
+
+  stack<node *>s;
+  node *current;
+  int n=0;
+  if(root==NULL)
+        return;
+
+  s.push(root);
+  current=root->left;
+
+  while(n<2)
+  {
+      if(s.empty())
+      {
+         n++;
+         if(n==2)
+            break;
+      }
+
+
+      while(current!=NULL)
+      {
+          s.push(current);
+          current=current->left;
+
+      }
+
+      current=s.top();
+      s.pop();
+      cout<<current->data<<" ";
+      current=current->right;
+
+  }
+}
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])
+{
+		int i;
+
+		cout<<"\n1.Add new node in BST ";
+        cout<<"\n2.Preorder ";
+		cout<<"\n3.Inorder ";
+		cout<<"\n4.Postorder ";
+		cout<<"\n5.Level Order Traversal";
+		cout<<"\n6.Inorder without resursion";
+		cout<<"\n0.Exit";
+
+	do
+	{
+		cout<<"\nEnter your choice ";
+		cin>>i;
+
+		switch(i)
+		{
+			case 1:add();
+				break;
+
+			case 2:preorder(root);
+				break;
+
+			case 3:inorder(root);
+				break;
+
+			case 4:postorder(root);
+				break;
+
+            case 5:LevelOrder(root);
+                break;
+
+            case 6:Inorder_without_recursion(root);
+                break;
+
+			case 0:break;
+			default:cout<<"\nEnter valid choice ";
+		}
+	}
+
+		while(i!=0);
+
+
+
+		return 0;
+
+	}
+
