048ba2ad501a1cba24fa101ae9d15a34eeb375e8
Tushar Mann <tusharmann29@gmail.com>
2016-07-23 13:40:47 +0530

    DFS done

--- Depth_First_Search/main.cpp
+++ Depth_First_Search/main.cpp
@@ -1,9 +1,83 @@
 #include <iostream>
+#include<list>
 
 using namespace std;
 
+class graph
+{
+private:
+    int v;
+    list<int> *adj;
+
+public:
+    graph(int v);
+    void addEdge(int v,int w);
+    void DFS(int s);
+    void DFSutil(int s,bool visited[]);
+
+};
+
+graph::graph(int v)
+{
+    this->v=v;
+    adj=new list<int>[v];
+
+}
+
+void graph::addEdge(int v,int w)
+{
+    adj[v].push_back(w);
+
+}
+
+void graph::DFSutil(int s,bool visited[])
+{
+    visited[s]=true;
+    cout<<s<<" ";
+
+    list<int>::iterator i;
+    for(i=adj[s].begin();i!=adj[s].end();i++)
+    {
+        if(!visited[*i])
+        {
+            DFSutil(*i,visited);
+
+        }
+    }
+
+}
+
+void graph::DFS(int s)
+{
+    bool *visited=new bool[v];
+    int j;
+    for(j=0;j<v;j++)
+        visited[j]=false;
+
+    DFSutil(s,visited);
+
+}
+
+
+
+
+
+
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    graph g(4);
+
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,2);
+    g.addEdge(2,0);
+    g.addEdge(2,3);
+    g.addEdge(3,3);
+
+    cout<<"\n"<<"Depth First Search of the given graph is ";
+    g.DFS(2);
+
     return 0;
 }
