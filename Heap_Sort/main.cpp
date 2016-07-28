a7d2a38fc9bbc4303f3281bd7e2487f39d3e4861
Tushar Mann <tusharmann29@gmail.com>
2016-07-28 21:51:56 +0530

    heap:

--- Heap_Sort/main.cpp
+++ Heap_Sort/main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int arr[100],i,j,n;
+    cout<<"\nEnter the size of array ";
+    cin>>n;
+    cout<<"\nEnter the elements in array ";
+    for(i=1;i<=n;i++)
+        cin>>arr[i];
+
+
     return 0;
 }
