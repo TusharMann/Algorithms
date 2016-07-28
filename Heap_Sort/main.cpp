170fa0c1fd2379d7cfb3c1a81a81e9d967686c83
Tushar Mann <tusharmann29@gmail.com>
2016-07-28 21:58:18 +0530

    max_heap in progress

--- Heap_Sort/main.cpp
+++ Heap_Sort/main.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+int hsize;
+
+void max_heap(int arr[],int n)
+{
+    int left=n*2;
+    int right=(n*2)+1;
+    int largest;
+
+    if()
+
+}
+
+void build_heap(int arr[],int n)
+{
+    hsize=n;
+    for(int i=n/2;i>0;i--)
+        max_heap(arr[i],i);
+
+
+}
+
 int main()
 {
     int arr[100],i,j,n;
@@ -11,6 +32,6 @@ int main()
     for(i=1;i<=n;i++)
         cin>>arr[i];
 
-
+    build_heap(arr,n);
     return 0;
 }
