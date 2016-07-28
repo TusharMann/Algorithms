82406b8de4f49e91455e142fdd08963b8dd31688
Tushar Mann <tusharmann29@gmail.com>
2016-07-28 22:14:18 +0530

    Output not coming right

--- Heap_Sort/main.cpp
+++ Heap_Sort/main.cpp
@@ -14,10 +14,10 @@ void max_heap(int arr[],int n)
         largest=left;
     else
         largest=n;
-    if(right<=hsize && arr[right>arr[largest])
+    if(right<=hsize && arr[right]>arr[largest])
         largest=right;
 
-    if(largest!=i)
+    if(largest!=n)
     {
         arr[n]=temp;
         arr[n]=arr[largest];
@@ -32,19 +32,22 @@ void build_heap(int arr[],int n)
 {
     hsize=n;
     for(int i=n/2;i>0;i--)
-        max_heap(arr[i],i);
+        max_heap(arr,i);
 
 
 }
 
 int main()
 {
-    int arr[100],i,j,n;
+    int arr[100],i,n;
     cout<<"\nEnter the size of array ";
     cin>>n;
     cout<<"\nEnter the elements in array ";
     for(i=1;i<=n;i++)
+    {
+        cout<<"\nEnter any number ";
         cin>>arr[i];
+    }
 
     build_heap(arr,n);
 
@@ -60,6 +63,10 @@ int main()
 
 
     }
+
+    cout<<"\nSorted array is ";
+    for(i=1;i<=n;i++)
+        cout<<"\t"<<arr[i];
 
 
     return 0;
