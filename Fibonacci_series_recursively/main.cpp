1132fbf12afaddcc75ffb42f22df91513aea5fe5
Tushar Mann <tusharmann29@gmail.com>
2016-08-04 23:10:00 +0530

    fibonacci

--- Fibonacci_series_recursively/main.cpp
+++ Fibonacci_series_recursively/main.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+int m;
+
+void fibonacci(int a,int b,int n)
+{
+    if(n<m)
+    {
+    cout<<"\t"<<a+b;
+    int c=a;
+    a=b;
+    b=c+b;
+    n++;
+    fibonacci(a,b,n);
+
+    }
+
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout<<"\nEnter the number of terms for fibonacci series ";
+    cin>>m;
+    cout<<"\t0\t1";
+
+    fibonacci(0,1,2);
+
     return 0;
 }
