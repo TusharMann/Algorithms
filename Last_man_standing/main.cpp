74c787c363d01efec5dfdc44c0f497cd62d1fcbc
Tushar Mann <tusharmann29@gmail.com>
2016-08-08 07:06:26 +0530

    still working

--- Last_man_standing/main.cpp
+++ Last_man_standing/main.cpp
@@ -4,6 +4,37 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+   int t;
+   cin>>t;
+
+   while(t--)
+   {
+      int *arr,*brr,i,n,m,j;
+      cin>>n;
+
+      arr=new int[n];
+      brr=new int[n];
+
+      for(i=1;i<=n;i++)
+        arr[i]=brr[i]=i;
+
+        m=n/2;
+
+        while(m>0)
+        {
+
+            for(i=1,j=2;i<=m;i++,j=j+2)
+            {
+                brr[i]=brr[j];
+            }
+            m=m/2;
+        }
+
+        cout<<brr[1];
+
+
+
+   }
+
     return 0;
 }
